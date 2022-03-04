1ddb5df25f80b35e11933e1848eee36133632948
alexhohom <alexhohom@outlook.com>
2022-03-04 22:11:51 +0800

    init repo

---
+++ src/Solution/question.h
@@ -0,0 +1,7 @@
+#include <vector>
+
+using std::vector;
+class Solution {
+public:
+    long long subArrayRanges(vector<int> &nums) { return 0; }
+};
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Solution/question.h"
+#include <iostream>
+
+using std::cout;
+using std::endl;
+int main() {
+    cout << "hello world" << endl;
+    Solution sol;
+    vector<int> vec;
+    cout << sol.subArrayRanges(vec) << endl;
+}
